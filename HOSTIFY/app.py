962c93affb146e714920911e6a79d283420a9bf5
TimTree <TimTree@users.noreply.github.com>
2019-11-19 12:38:51 -0800

    Adjust inputs to match backend schema

--- HOSTIFY/app.py
+++ HOSTIFY/app.py
@@ -11,14 +11,14 @@ def create_app():
 
     @app.route('/input', methods=['POST'])
     def retrieval():
-        neighbourhood_group = request.values['neighbourhood_group']
-        neighbourhood = request.values['neighbourhood']
+        neighbourhood_group = request.values['neighborhood_group']
+        neighbourhood = request.values['neighborhood']
         room_type = request.values['room_type']
         minimum_nights = request.values['minimum_nights']
         calculated_host_listings_count = request.values['calculated_host_listings_count']
-        availability_365 = request.values['availability_365']
-        bathrooms = request.values['bathrooms']
-        bedrooms = request.values['bedrooms']
+        availability_365 = request.values['availability_of_year']
+        bathrooms = request.values['bathroom_number']
+        bedrooms = request.values['bedroom_number']
         output = "{}, {}, {}, {}, {}, {}, {}, {}" \
             .format(neighbourhood_group, neighbourhood, \
                    room_type, minimum_nights, calculated_host_listings_count, \
