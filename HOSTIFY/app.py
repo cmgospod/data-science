e77341167e33f25fb4d1f75145bc7dfa619228b6
TimTree <TimTree@users.noreply.github.com>
2019-11-20 14:00:02 -0800

    Process the given neighborhoods/room types

--- HOSTIFY/app.py
+++ HOSTIFY/app.py
@@ -15,9 +15,9 @@ def create_app():
 
     @app.route('/input', methods=['POST'])
     def retrieval():
-        neighbourhood_group = request.values['neighborhood_group']
-        neighbourhood = request.values['neighborhood']
-        room_type = request.values['room_type']
+        neighbourhood_group = int(request.values['neighborhood_group'])
+        neighbourhood = int(request.values['neighborhood'])
+        room_type = int(request.values['room_type'])
         minimum_nights = int(request.values['minimum_nights'])
         calculated_host_listings_count = int(request.values['calculated_host_listings_count'])
         availability_365 = int(request.values['availability_of_year'])
@@ -26,14 +26,10 @@ def create_app():
 
         predict_thing = pd.DataFrame(columns=['neighbourhood_group', 'neighbourhood', 'room_type', 'minimum_nights',
        'calculated_host_listings_count', 'availability_365', 'bathrooms',
-       'bedrooms'], data=[[1,1,1,minimum_nights,calculated_host_listings_count,availability_365,bathrooms,bedrooms]])
+       'bedrooms'], data=[[neighbourhood_group,neighbourhood,room_type,minimum_nights,calculated_host_listings_count,availability_365,bathrooms,bedrooms]])
 
         prediction = pipeline.predict(predict_thing)[0].round(2)
 
-        #output = "{}, {}, {}, {}, {}, {}, {}, {}" \
-        #    .format(neighbourhood_group, neighbourhood, \
-        #           room_type, minimum_nights, calculated_host_listings_count, \
-        #           availability_365, bathrooms, bedrooms)
         return str(prediction)
     
     return app
