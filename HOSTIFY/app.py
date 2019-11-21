6b4fc4cd38621cddc58b5070a9f1d5de365168d6
Nckflannery <Nckflannery@users.noreply.github.com>
2019-11-21 12:19:54 -0800

    Output String Formatting done

--- HOSTIFY/app.py
+++ HOSTIFY/app.py
@@ -41,7 +41,7 @@ def create_app():
             if low_range < 1:
                 low_range = 1
             high_range = prediction+error
-            price_range = 'Predicted price range for your listing is:'
+            price_range = 'Predicted price range for your listing is: '
             return price_range + f'€{low_range}-€{high_range}'
         except Exception as e:
             errorMessage = "Error processing input: {}".format(e)
